07a8bd76f1156ac3424f01f3f4937cef1082d6ea
Aleksandr <mes-aleksandr@yandex.ru>
2021-01-14 17:02:46 +0300

    create header

--- src/components/Landing/Header/Header.jsx
+++ src/components/Landing/Header/Header.jsx
@@ -3,10 +3,10 @@ import "./Header.css";
 
 function Header(props) {
   return (
-    <>
-    <div className="header-top"></div>
-      <div className="bg-blue-100">Taiilwindcss connected!!</div>
-      </>
+    <div className="flex flex-row flex-wrap justify-center">
+    <div className="w-full h-80">gggg</div>
+      <div className="bg-blue-100">Tailwindcss connected!!</div>
+      </div>
   )
 };
 
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -5,9 +5,9 @@ function Landing() {
 const [name, setName] = useState("Alex");
 
   return (
-    <>
+    <div className="container p-0 ml-0 mr-auto">
       <Header></Header>
-    </>
+    </div>
   );
 };
 
