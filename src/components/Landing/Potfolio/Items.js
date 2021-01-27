5f3c08db57ed16d639dfb5c7b0b8f9761da3291c
Aleksandr <mes-aleksandr@yandex.ru>
2021-01-28 02:00:32 +0300

    section portfolio slider

---
+++ src/components/Landing/Potfolio/Items.js
@@ -0,0 +1,7 @@
+import img1 from "./img/interior.png";
+import img2 from "./img/logotype.png";
+import img3 from "./img/waxom.jpg";
+
+const listItems = [img1, img2, img3];
+
+export default listItems;
--- src/components/Landing/Potfolio/Portfolio.jsx
+++ src/components/Landing/Potfolio/Portfolio.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import Slider from "../Slider";
+import listItems from "./Items.js";
 
 function Portfolio(props) {
+  const Items = listItems;
+  const [active, setActive] = useState();
+  
   return (
     <>
-      <div>
-        <h3>{props.title}</h3>
+      <div className="section-team flex flex-row flex-wrap justify-center w-full p-10 mt-10">
+        <h3 className="section-team-title w-full flex justify-center font-medium uppercase text-center text-3xl text-gray-300 leading-7 tracking-wider">
+          {props.title}
+        </h3>
+        <Slider initial={active} data={Items} />
       </div>
     </>
   );
