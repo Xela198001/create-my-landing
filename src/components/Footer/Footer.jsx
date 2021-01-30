e8d40a74fc1616ca6877df7ca72cbfae2fc5f27c
Aleksandr <mes-aleksandr@yandex.ru>
2021-01-30 14:19:10 +0300

    create footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Footer(params) {
+  return (
+    <>
+      <div className="section-team flex flex-row flex-wrap justify-center w-full p-10 mt-10">
+        
+      </div>
+    </>
+  );
+}
+};
